ebefc1730800515e7df4db9c3e3ccccc536dd10d
Maciej Kołodziej <makciek@gmail.com>
2024-09-11 16:44:36 +0200

    Volume up

--- Hackathon_Neworbit/NativeFunctions.cs
+++ Hackathon_Neworbit/NativeFunctions.cs
@@ -11,7 +11,17 @@ public class NativeFunctions
     {
         var previousColor = Console.ForegroundColor;
         Console.ForegroundColor = ConsoleColor.Red;
-        Console.WriteLine("I don't know");
+        Console.WriteLine("There was a problem with your request, I'll reach out for help to the human :)");
+        Console.ForegroundColor = previousColor;
+    }
+
+    [KernelFunction]
+    [Description("Handle the issue with sound and the microphone")]
+    public static void VolumeUpAndUnmute()
+    {
+        var previousColor = Console.ForegroundColor;
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Volume up and unmute");
         Console.ForegroundColor = previousColor;
     }
 }
